61dc456dad49ebc83175c19bb468fb4dfeb5fa82
charles <charles.chen@jwaoo.com>
2018-05-25 15:49:26 +0800

    1、修改配置文件。

--- genConf.sh
+++ genConf.sh
@@ -13,3 +13,29 @@ else
     echo "$dstFile is exsited"
 
 fi
+
+
+if [ ! -f /lib/systemd/system/avchat-apprtc.service ]; then
+
+	echo "copy avchat-apprtc.service ...."
+	cp ./conf/avchat-apprtc.service /lib/systemd/system/
+
+else
+	echo "avchat-apprtc.service is existed"
+
+fi
+
+if [ ! -f /usr/bin/avchat-apprtc-server ]; then
+
+	echo "copy aavchat-apprtc-server ...."
+	cp ./conf/avchat-apprtc-server /usr/bin/
+else
+	echo "avchat-apprtc-server is existed"
+
+fi
+
+
+echo "success"
+
+
+
